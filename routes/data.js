a62f2c4e7029a4e5c280b9c5c96235fc8129684d
Beibhinn <beibhinn.keenan@gmail.com>
2020-05-28 19:02:37 +0100

    Data endpoint adds to database and does not allow duplicates

--- routes/data.js
+++ routes/data.js
@@ -1,22 +1,28 @@
-
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const SensorData = require('../models.js').SensorData;
 
 /* PUT data */
 router.put('/', [
     check("sensorId").isString().notEmpty(),
     check("time").isInt(),
     check("value").isFloat(),
-], function(req, res, next) {
-
+], async function(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log(req.body);
-    return res.status(204);
+    // Don't allow duplicate records - sensor Id & time pairings must be unique
+    if (await SensorData.findOne({ where: { sensorId: req.body.sensorId, time: req.body.time } })) {
+        return res.status(409).send();
+    }
+
+    // Create new database entry
+    await SensorData.create({ sensorId: req.body.sensorId, time: req.body.time, value: req.body.value });
+
+    return res.status(204).send();
 });
 
 module.exports = router;
