160ea8d202a36741195da8a06f49e437214a3143
Beibhinn <beibhinn.keenan@gmail.com>
2020-05-29 08:49:44 +0100

    Add alert put and get endpoints

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var dataRouter = require('./routes/data');
+var alertRouter = require('./routes/alerts');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/data', dataRouter);
+app.use('/alerts', alertRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/alerts.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const Op = require('sequelize').Op;
+const SensorData = require('../models.js').SensorData;
+const Alerts = require('../models.js').Alerts;
+
+
+/* PUT data */
+router.put('/', [
+    check("sensorId").isString().notEmpty(),
+    check("upperThreshold").isFloat(),
+    check("lowerThreshold").isFloat(),
+], async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    // Create new alerts database entry for a particular sensor ID, or update if one for that ID already exists
+    await Alerts.upsert({
+        sensorId: req.body.sensorId,
+        upperThreshold: req.body.upperThreshold,
+        lowerThreshold: req.body.lowerThreshold
+    });
+
+    return res.status(204).send();
+});
+
+
+/* GET data. */
+router.get('/', [
+    check("sensorId").isString().notEmpty()
+], async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    // find all thresholds for a particular sensor id.
+    const results = await Alerts.findOne({
+        where: {
+            sensorId: req.body.sensorId,
+        }
+    });
+    if (results) {
+        return res.json(results);
+    }
+    return res.status(404).send(`No alerts found for sensor id: ${req.body.sensorId}`);
+});
+
+module.exports = router;
