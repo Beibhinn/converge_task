ef012505d7bf7285d9214e68988b6c77dcc7fbfe
Beibhinn <beibhinn.keenan@gmail.com>
2020-05-30 13:25:00 +0100

    Add Postman collection
    & clean up project. Remove unused files.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var { dataRouter, setThresholdCallback } = require('./routes/data');
 var alertRouter = require('./routes/alerts');
 
@@ -13,17 +12,12 @@ setThresholdCallback((sensorData) => console.log(`Alert: ${JSON.stringify(sensor
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/data', dataRouter);
 app.use('/alerts', alertRouter);
 
